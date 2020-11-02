ec80ce95e6cb3764da1745acc4376817faa25956
Josep Jesus <josep@prowarehouse.nl>
2020-11-02 18:43:56 +0100

    Add magical line to enable overall font ligatures

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 set theme_color_scheme gruvbox
+set LC_CTYPE en_US.UTF-8
 set PATH /Users/joe/Development/SDKs/go1.15.2/bin $PATH
 set PATH /Applications/MAMP/bin/php/php7.4.2/bin $PATH
 set PATH /Users/joe/Development/SDKs/go/bin $PATH
